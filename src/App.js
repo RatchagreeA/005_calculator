d326cbeb524f44379af9bc066898f707cc67e0ba
RatchagreeA <ratchagree@gmail.com>
2022-06-28 22:39:29 +0700

    update layout

--- src/App.js
+++ src/App.js
@@ -4,17 +4,9 @@ function App() {
     return (
         <div className="App">
             <div className="calculator">
-                <div className="keypad">
-                    {btns.map((btn) => (
-                        <button
-                            id={btn.id}
-                            value={btn.val}
-                            style={{ gridArea: btn.id }}
-                        >
-                            {btn.val}
-                        </button>
-                    ))}
-                </div>
+                <FormularScreen />
+                <Display />
+                <KeyPad />
             </div>
         </div>
     );
@@ -22,34 +14,49 @@ function App() {
 
 export default App;
 
-function KeyPAd() {
-    return <div>{}</div>;
+function FormularScreen() {
+    return <div className="formularScreen">{"1+2+3+5+5"}</div>;
 }
-
-function Key() {
+function Display() {
     return (
-        <div>
-            <button>{}</button>
+        <div id="display" className="displayScreen">
+            {"12345"}
+        </div>
+    );
+}
+function KeyPad() {
+    return (
+        <div className="keypad">
+            {btns.map((btn) => (
+                <button
+                    id={btn.id}
+                    className={btn.class}
+                    value={btn.val}
+                    style={{ gridArea: btn.id }}
+                >
+                    {btn.val}
+                </button>
+            ))}
         </div>
     );
 }
 
 const btns = [
-    { id: "zero", val: "0" },
-    { id: "one", val: "1" },
-    { id: "two", val: "2" },
-    { id: "three", val: "3" },
-    { id: "four", val: "4" },
-    { id: "five", val: "5" },
-    { id: "six", val: "6" },
-    { id: "seven", val: "7" },
-    { id: "eight", val: "8" },
-    { id: "nine", val: "9" },
-    { id: "add", val: "+" },
-    { id: "subtract", val: "-" },
-    { id: "multiply", val: "X" },
-    { id: "divide", val: "/" },
-    { id: "equals", val: "=" },
-    { id: "decimal", val: "." },
-    { id: "clear", val: "AC" },
+    { id: "zero", val: "0", class: "num" },
+    { id: "one", val: "1", class: "num" },
+    { id: "two", val: "2", class: "num" },
+    { id: "three", val: "3", class: "num" },
+    { id: "four", val: "4", class: "num" },
+    { id: "five", val: "5", class: "num" },
+    { id: "six", val: "6", class: "num" },
+    { id: "seven", val: "7", class: "num" },
+    { id: "eight", val: "8", class: "num" },
+    { id: "nine", val: "9", class: "num" },
+    { id: "add", val: "+", class: "oper" },
+    { id: "subtract", val: "-", class: "oper" },
+    { id: "multiply", val: "X", class: "oper" },
+    { id: "divide", val: "/", class: "oper" },
+    { id: "equals", val: "=", class: "equal" },
+    { id: "decimal", val: ".", class: "num" },
+    { id: "clear", val: "AC", class: "clear" },
 ];
