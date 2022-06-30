b7e860f632382439bbdfdfbac002866fc95d5177
RatchagreeA <ratchagree@gmail.com>
2022-06-30 23:56:03 +0700

    update cal fucntion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-    const [formular, setFormular] = useState("");
+    const [formula, setFormula] = useState("");
     const [curVal, setCurVal] = useState("0");
     const [prevVal, setPrevVal] = useState("");
     const [evalStatus, setEvalStatus] = useState(false);
@@ -11,69 +11,129 @@ function App() {
         let tmpVal = curVal;
         setCurVal("Digit Limit Met");
         setTimeout(() => setCurVal(tmpVal), 1000);
+        setPrevVal(curVal);
     };
     const clear = () => {
-        setFormular("");
-        setCurVal("");
+        setFormula("");
+        setCurVal("0");
+        setPrevVal("0");
         setEvalStatus(false);
     };
     const evaluated = () => {
-        const result = eval(formular).toString();
-        setCurVal(result);
-        setFormular(formular + "=" + result);
+        let tmpFormula = formula.replace("--", "+0+0+0+0+0+0+");
+        while (endsWithOperator.test(tmpFormula)) {
+            tmpFormula = tmpFormula.slice(0, -1);
+        }
+        const result =
+            Math.round(1000000000000 * eval(tmpFormula)) / 1000000000000;
+        tmpFormula = formula.replace("+0+0+0+0+0+0+", "--");
+        const textResult = result.toString();
+        setCurVal(textResult);
+        setFormula(tmpFormula + "=" + textResult);
         setEvalStatus(true);
-        setPrevVal(result);
+        setPrevVal(textResult);
     };
 
-    const handleNumbers = (e) => {
-        const val = e.target.value;
-
-        if (!curVal.includes("Limit")) {
-            if (val === "AC") {
-                clear();
-            } else if (evalStatus) {
-                if (isOperator.test(val)) {
-                    setFormular(prevVal + val);
+    const handleOperators = (val) => {
+        setCurVal(val);
+        if (evalStatus) {
+            setFormula(prevVal + val);
+            setEvalStatus(false);
+        } else if (!endsWithOperator.test(formula)) {
+            setFormula(formula + val);
+            setPrevVal(formula);
+        } else if (!endsWithNegativeSign.test(formula)) {
+            if (endsWithNegativeSign.test(formula + val)) {
+                setFormula(formula + val);
+            } else {
+                setFormula(prevVal + val);
+            }
+        } else if (val !== "-") {
+            setFormula(prevVal + val);
+        }
+    };
+    const handleNumbers = (val) => {
+        if (evalStatus) {
+            if (val === "0") {
+                setFormula("");
+            } else {
+                setFormula(val);
+            }
+            setCurVal(val);
+            setEvalStatus(false);
+        } else {
+            if (curVal.length > 21) {
+                maxDigitWarning();
+            } else {
+                if (isOperator.test(curVal) || curVal === "0") {
                     setCurVal(val);
                 } else {
-                    setFormular(val);
-                    setCurVal(val);
+                    setCurVal(curVal + val);
                 }
-                setEvalStatus(false);
-            } else if (val === "=") {
+                if (val === "0" && curVal === "0") {
+                    if (formula === "") {
+                        setFormula("0");
+                    } else {
+                        setFormula(formula);
+                    }
+                } else {
+                    if (/([^.0-9]0|^0)$/.test(formula)) {
+                        setFormula(formula.slice(0, -1) + val);
+                    } else {
+                        setFormula(formula + val);
+                    }
+                }
+            }
+        }
+    };
+    const handleDecimal = (val) => {
+        if (evalStatus) {
+            setFormula("0" + val);
+            setCurVal("0" + val);
+            setEvalStatus(false);
+        } else {
+            if (curVal.length > 21) {
+                maxDigitWarning();
+            } else {
+                if (curVal.includes(".")) {
+                    setCurVal(curVal);
+                    setFormula(formula);
+                } else {
+                    setCurVal(curVal + val);
+                    if (formula === "" || endsWithOperator.test(formula)) {
+                        setFormula(formula + "0" + val);
+                    } else {
+                        setFormula(formula + val);
+                    }
+                }
+            }
+        }
+    };
+
+    const handleOnClick = (e) => {
+        if (!curVal.includes("Limit")) {
+            const val = e.target.value;
+            if (isOperator.test(val)) {
+                handleOperators(val);
+            } else if (isNumbers.test(val)) {
+                handleNumbers(val);
+            } else if (isDeci.test(val)) {
+                handleDecimal(val);
+            } else if (isClear.test(val)) {
+                clear();
+            } else if (isEvaluated.test(val)) {
                 evaluated();
             } else {
-                if (curVal.length > 21 || formular.length > 21) {
-                    maxDigitWarning();
-                } else if (isOperator.test(val)) {
-                    if (endsWithOperator.test(formular)) {
-                        setFormular(formular.slice(0, -1) + val);
-                        setCurVal(val);
-                    } else {
-                        setFormular(formular + val);
-                        setCurVal(val);
-                    }
-                } else {
-                    if (isOperator.test(curVal) || curVal === "0") {
-                        setCurVal(val);
-                    } else {
-                        setCurVal(curVal + val);
-                    }
-                    if (val === "0" && curVal === "0") {
-                        setFormular(formular);
-                    } else {
-                        setFormular(formular + val);
-                    }
-                }
+                //pass
             }
         }
     };
     return (
         <div className="App">
             <div className="calculator">
-                <FormularScreen formular={formular.replace(/x/g, "⋅")} />
+                <FormulaScreen formula={formula.replace(/\*/g, "⋅")} />
                 <Display curVal={curVal} />
-                <KeyPad handleNumbers={handleNumbers} />
+                <KeyPad handleOnClick={handleOnClick} />
             </div>
         </div>
     );
@@ -81,8 +141,8 @@ function App() {
 
 export default App;
 
-function FormularScreen({ formular }) {
-    return <div className="formularScreen">{formular}</div>;
+function FormulaScreen({ formula }) {
+    return <div className="formulaScreen">{formula}</div>;
 }
 function Display({ curVal }) {
     return (
@@ -91,7 +151,7 @@ function Display({ curVal }) {
         </div>
     );
 }
-function KeyPad({ handleNumbers }) {
+function KeyPad({ handleOnClick }) {
     return (
         <div className="keypad">
             {btns.map((btn) => (
@@ -100,7 +160,7 @@ function KeyPad({ handleNumbers }) {
                     className={btn.class}
                     value={btn.val}
                     style={{ gridArea: btn.id }}
-                    onClick={handleNumbers}
+                    onClick={handleOnClick}
                 >
                     {btn.val}
                 </button>
@@ -108,9 +168,13 @@ function KeyPad({ handleNumbers }) {
         </div>
     );
 }
-const isOperator = /[x/+-]/;
-const endsWithOperator = /[x+-/]$/;
-const endsWithNegativeSign = /\d[x/+-]{1}-$/;
+const isOperator = /[\*/+-]/;
+const isNumbers = /[0-9]/;
+const isClear = /AC/;
+const isDeci = /\./;
+const isEvaluated = /\=/;
+const endsWithOperator = /[\*+-/]$/;
+const endsWithNegativeSign = /\d[\*/+-]{1}-$/;
 
 const btns = [
     { id: "zero", val: "0", class: "num" },
@@ -125,7 +189,7 @@ const btns = [
     { id: "nine", val: "9", class: "num" },
     { id: "add", val: "+", class: "oper" },
     { id: "subtract", val: "-", class: "oper" },
-    { id: "multiply", val: "x", class: "oper" },
+    { id: "multiply", val: "*", class: "oper" },
     { id: "divide", val: "/", class: "oper" },
     { id: "equals", val: "=", class: "equal" },
     { id: "decimal", val: ".", class: "num" },
